775f2f2d9b32fc2c3381a8456a908fead5ab80f9
Gabriel Duarte <biel.dbastos@gmail.com>
2023-02-27 17:37:36 -0300

    Adicionando arquivios php

---
+++ php/Laravel - criando uma aplicação com MVC/controle-serie/resources/views/series/edit.blade.php
@@ -0,0 +1,7 @@
+<x-layout title="Editar Série {{ $series->nome }}">
+    <x-series.form 
+        :action="route('series.update', $series->id)" 
+        :nome="$series->nome" 
+        :update=true
+    />
+</x-layout>
